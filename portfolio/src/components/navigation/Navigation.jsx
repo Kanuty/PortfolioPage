24b1583e93c15894931d7a27fcc70e0d46db8862
Kanuty <bartoszdudek2211@gmail.com>
2020-09-08 15:37:48 +0200

    T-002
    add style to navigation

--- portfolio/src/components/navigation/Navigation.jsx
+++ portfolio/src/components/navigation/Navigation.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-
+import { faAddressCard, faDiceD20, faGem, faHome, faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import "./style.module.css"
 
 const Navigation = (props) => {
@@ -16,20 +16,33 @@ const Navigation = (props) => {
   return (
   <header>
     <img src={logo} alt='logo'></img>
-
-
+    <nav>
+      <a rel="index" target="_blank">
+        <FontAwesomeIcon icon={faHome} />
+      </a>
+      <a rel="about" target="_blank">
+        <FontAwesomeIcon icon={faUserSecret} />
+      </a>
+      <a rel="skills" target="_blank">
+        <FontAwesomeIcon icon={faDiceD20} />
+      </a>
+      <a rel="gallery" target="_blank">
+        <FontAwesomeIcon icon={faGem} />
+      </a>
+      <a rel="contact" target="_blank">
+        <FontAwesomeIcon icon={faAddressCard} />
+      </a>
+    </nav>
     <ul>
       <li>
         <a href={linkedinLink} target="_blank">
           <FontAwesomeIcon icon={faLinkedin} />
-
         </a>
       </li>
       <li>
         <a href={facebookLink} target="_blank">
           <FontAwesomeIcon icon={faFacebook} />
         </a>
-        
       </li>
     </ul>
   </header>

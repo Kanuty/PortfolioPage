d27854c8b1bae758b2335543b33d2d2ecb3bcff5
Kanuty <bartoszdudek2211@gmail.com>
2020-09-22 18:49:52 +0200

    T-009
    New pictures and animations for skills view

--- portfolio/src/components/conteners/cyberpunk02/Cyberpunk02.jsx
+++ portfolio/src/components/conteners/cyberpunk02/Cyberpunk02.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import html from './img/html002.svg';
+import css from './img/css002.svg';
+import js from './img/js002.svg';
+import react from './img/html001.svg';
+import redux from './img/html001.svg';
 
-import { container, upperTrivia, lowerTrivia, mainContent, skillEmblem, skillText} from'./style.module.css';
+import { container, upperTrivia, mainContent, skillEmblem, skillText} from'./style.module.css';
 
 function Cyberpunk02(props) {
   const {
     text,
   } = props;
+
+
+  const image = text === "HTML" ? html :
+    text ==="CSS" ? css :
+    text ==="JS" ? js :
+    text ==="REACT" ? react :
+    text ==="REDUX" ? redux :
+    html;
   
   return (
     <div className={container}>
@@ -15,26 +28,24 @@ function Cyberpunk02(props) {
         </li>
         <li className={mainContent}>
           <div className={skillEmblem}>
+            <img src={image} alt={text}></img>
           </div>
           <div className={skillText}>
             <span>{text}</span>
           </div>
         </li>
-
       </ul>
-
     </div>
   );
 }
 
 Cyberpunk02.propTypes = {
   text: PropTypes.string,
-  container: PropTypes.string,
+
 };
 
 Cyberpunk02.defaultProps = {
   text: `Lorem ipsum`,
-  container: "container",
 };
 
 export default (Cyberpunk02);

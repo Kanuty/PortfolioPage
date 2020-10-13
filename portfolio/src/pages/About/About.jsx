46042c4b5d89026312d71e9e59c0b0e8f45035fc
Kanuty <bartoszdudek2211@gmail.com>
2020-10-13 19:46:34 +0200

    ToDo
    Ideas about what About page shoud contain

--- portfolio/src/pages/About/About.jsx
+++ portfolio/src/pages/About/About.jsx
@@ -10,6 +10,12 @@ function About() {
       <GlitchedText text="About"></GlitchedText>
       <h3>Under construction</h3>
       <Cyberpunk01 ></Cyberpunk01>
+      <p>Things to do:
+        Links to Artstation and Gitlab
+        What I do right now
+        My non-programming expertes
+
+      </p>
     </div>
   );
 }

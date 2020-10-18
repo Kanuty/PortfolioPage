9faf9432ae23362394e265fbc93340c3e65d44df
Kanuty <bartoszdudek2211@gmail.com>
2020-10-18 15:49:47 +0200

    T-20
    hot-fix, purrification of  wrong-doings in Home->description

--- portfolio/src/pages/Home/Home.jsx
+++ portfolio/src/pages/Home/Home.jsx
@@ -12,15 +12,15 @@ function Home() {
       <div className={homeContainer}>
         <h3>Bartosz "Kanut" Dudek</h3>
         <h5>Front-end Developer (React)</h5>
-        <p className={description}>My current goal is to became capable and self-sufficent web developer.
+        <p className={description}>My current goal is to become capable and self-sufficient web developer.
           I want to create web pages and applications that will improve life on daily basis.
           Tools for RPG games? Of course! Social platforms for self development? Sure!
           
-          But main main goal is to make proffesional tools for academic that will help
+          But main main goal is to make professional tools for academic that will help
           research and show everything related to international studies.
-          Ideas, people, teritory... sociology, military, economics, politics...
-          World is changing  more rapid then JS frameworks. Without modern tools
-          we will not be able to face future prepered. </p>
+          Ideas, people, territory... sociology, military, economics, politics...
+          World is changing more rapidly then JS frameworks. Without modern tools
+          we will not be able to face future prepared. </p>
         <img src={require('./img/timeline.svg')} alt='Timeline'></img>
         <p className={`${description} ${note}`}>*KSSM  UJ = International Relations Student’s Association at Jagiellonian University </p>
         <p className={description}>If you have a job offer, or you  want to help me grow as a developer in another way than:</p>
